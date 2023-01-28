7a0361160e13e4e88ceb39fa6fe73295d4c4c9c4
cramke <carsten.ramke@gmail.com>
2023-01-28 08:58:21 +0100

    Add unittests for default optimizer

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -8,6 +8,7 @@ pub trait Optimizer {
 #[derive(Debug, Copy, Clone)]
 pub struct DefaultOptimizer;
 impl Optimizer for DefaultOptimizer {
+
     fn get_edge_weight(&self, begin: &Node2D, end: &Node2D) -> f64 {
         let a = (begin.x - end.x).powi(2);
         let b: f64 = (begin.y - end.y).powi(2);
@@ -18,5 +19,35 @@ impl Optimizer for DefaultOptimizer {
 
     fn init(&self) -> bool {
         return true;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::node::Node2D;
+    use super::{Optimizer, DefaultOptimizer};
+
+    #[test]
+    fn test_default_init() {
+        let optimizer = DefaultOptimizer;
+        assert_eq!(true, optimizer.init());
+    }
+
+    #[test]
+    fn test_default_edge_weight_x() {
+        let optimizer = DefaultOptimizer;
+        let a = Node2D::new(0f64, 0f64);
+        let b = Node2D::new(1f64, 0f64);
+
+        assert_eq!(1f64, optimizer.get_edge_weight(&a, &b));
+    }
+
+    #[test]
+    fn test_default_edge_weight_y() {
+        let optimizer = DefaultOptimizer;
+        let a = Node2D::new(0f64, 0f64);
+        let b = Node2D::new(0f64, 1f64);
+
+        assert_eq!(1f64, optimizer.get_edge_weight(&a, &b));
     }
 }
