22d0a81eb3504db743063eb4dd018201829df8b0
cramke <carsten.ramke@gmail.com>
2023-02-04 17:26:48 +0100

    Add CollisionChecker based on geo

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,25 +1,11 @@
-use mpf::{self, problem::{ProblemDefinition, Parameter}, optimizer::{DefaultOptimizer, Optimizer}, collision_checker::{CollisionChecker, NaiveCollisionChecker}};
+use geo::{Contains, Intersects};
+use mpf::{self, problem::{ProblemDefinition, Parameter}, optimizer::{DefaultOptimizer, Optimizer}, collision_checker::{CollisionChecker, NaiveCollisionChecker}, boundaries::Boundaries};
 
 #[test]
 fn test_mpf_default_scenario() {
     use mpf;
     use mpf::{boundaries::Boundaries};
-    use geo_types::Point;
-
-    fn is_collision(node: &Point) -> bool {
-        if node.x() > 1.0 && node.x() < 2.0 {
-            return true;
-        }
-        if node.y() > 1.0 && node.y() < 2.0 {
-            return true;
-        }
-    
-        return false;
-    }
-    
-    fn is_edge_in_collision() -> bool {
-        return false;
-    }
+    use geo::Point;
 
     let start: Point = Point::new(0f64, 0f64);
     let goal: Point = Point::new(3f64, 3f64);
@@ -32,34 +18,67 @@ fn test_mpf_default_scenario() {
     let cost = pdef.get_solution_cost();
     assert!(cost > 4f64);
     assert!(cost < 5f64);
-
 }
 
 
 #[test]
 fn test_mpf_naiv_scenario() {
     use mpf;
-    use mpf::{node::Node2D, boundaries::Boundaries};
+    use mpf::boundaries::Boundaries;
     use geo_types::Point;
-
-    fn is_collision(node: &Point) -> bool {
-        return false;
-    }
-    
-    fn is_edge_in_collision() -> bool {
-        return false;
-    }
 
     let start: Point = Point::new(0f64, 0f64);
     let goal: Point = Point::new(3f64, 3f64);
     let bounds: Boundaries = Boundaries::new(0f64, 3f64, 0f64, 3f64);
     let optimizer: Box<dyn Optimizer> = Box::new(DefaultOptimizer);
-    let params = Parameter::new(18usize);
+    let params = Parameter::new(25usize);
     let cc: Box<dyn CollisionChecker> = Box::new(NaiveCollisionChecker{});
     let mut pdef: ProblemDefinition = ProblemDefinition::new( start, goal, bounds, optimizer, params, cc);                                       
     pdef.solve();
     let cost: f64 = pdef.get_solution_cost();
     println!("{}", cost);
-    assert!(cost < 4.5f64);
+    assert!(cost < 4.7f64);
+}
+
+#[test]
+fn test_geo_collision() {
+    use geo::{Point, LineString, Polygon};
+
+    pub struct GeoCollisionChecker {
+        poly: Polygon,
+    }
     
+    impl CollisionChecker for GeoCollisionChecker {
+        fn init(&self) -> bool {
+            return true;
+        }
+    
+        fn is_edge_colliding(&self, begin: &Point, end: &Point) -> bool {
+            let a = Point::new(begin.x(), begin.y());
+            let b = Point::new(end.x(), end.y());
+            let line = LineString::from(vec![a, b]);
+            return self.poly.intersects(&line);
+        }
+    
+        fn is_node_colliding(&self, node: &Point) -> bool {
+            return self.poly.contains(node);
+        }
+    }
+
+    let poly = Polygon::new(
+    LineString::from(vec![(1., 1.), (2., 1.), (2., 2.), (1., 2.)]),
+    vec![],
+    );
+
+    let cc: Box<dyn CollisionChecker> = Box::new(GeoCollisionChecker{poly});
+    let start: Point = Point::new(0f64, 0f64);
+    let goal: Point = Point::new(3f64, 3f64);
+    let bounds: Boundaries = Boundaries::new(0f64, 3f64, 0f64, 3f64);
+    let optimizer: Box<dyn Optimizer> = Box::new(DefaultOptimizer);
+    let params = Parameter::new(30usize);
+    let mut pdef: ProblemDefinition = ProblemDefinition::new( start, goal, bounds, optimizer, params, cc);                                       
+    pdef.solve();
+    let cost: f64 = pdef.get_solution_cost();
+    println!("{}", cost);
+    assert!(cost > 4.3f64);
 }
