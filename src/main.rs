086f0db8e18ecb416fe57f7cbde77762e3562703
cramke <carsten.ramke@gmail.com>
2023-02-04 17:39:53 +0100

    Add Print during main run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use mpf::collision_checker::{CollisionChecker, NaiveCollisionChecker};
 use mpf::boundaries::Boundaries;
 use mpf::optimizer::{self, Optimizer};
@@ -10,10 +12,16 @@ fn main() {
     let goal: Point = Point::new(3f64, 3f64);
     let bounds: Boundaries = Boundaries::new(0f64, 3f64, 0f64, 3f64);
     let optimizer: Box<dyn Optimizer> = Box::new(optimizer::DefaultOptimizer);
-    let params = Parameter::new(50usize);
+    let params = Parameter::new(10usize);
     let cc: Box<dyn CollisionChecker> = NaiveCollisionChecker::new();
-    let mut pdef= ProblemDefinition::new( start, goal, bounds, optimizer, params, cc);                                       
+    let mut pdef= ProblemDefinition::new( start, goal, bounds, optimizer, params, cc);    
+
+    println!("#### mpf ####");
+    let start = Instant::now();
     pdef.solve();
+    let duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+
     let path: &str = "./examples/example1/graph.dot";
     pdef.print_statistics(path);
     let path: &str = "./examples/example1/solution_path.txt";
